fb560dce1a7d48f3aa44d24252c6ad6b61be78cd
bardaev <badma3581@gmail.com>
2020-04-21 02:31:45 +0300

    hw fix

--- src/main/java/ru/mail/polis/ImplDAO.java
+++ src/main/java/ru/mail/polis/ImplDAO.java
@@ -10,22 +10,22 @@ import java.util.TreeMap;
 
 public class ImplDAO implements DAO {
 
-    private SortedMap<ByteBuffer, Record> map = new TreeMap<>();
+    private final SortedMap<ByteBuffer, Record> map = new TreeMap<>();
 
     @NotNull
     @Override
-    public Iterator<Record> iterator(@NotNull ByteBuffer from) throws IOException {
-        Iterator<Record> iterator = map.tailMap(from).values().iterator();
+    public Iterator<Record> iterator(@NotNull final ByteBuffer from) throws IOException {
+        final Iterator<Record> iterator = map.tailMap(from).values().iterator();
         return iterator;
     }
 
     @Override
-    public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value) throws IOException {
+    public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) throws IOException {
         map.put(key, Record.of(key, value));
     }
 
     @Override
-    public void remove(@NotNull ByteBuffer key) throws IOException {
+    public void remove(@NotNull final ByteBuffer key) throws IOException {
         map.remove(key);
     }
 
